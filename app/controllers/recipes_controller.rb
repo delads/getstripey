deb397253e3fb0034a208d5d7ced829db7c7f601
delads <don@delads.com>
2016-07-17 16:46:19 +0000

    create chefs resources and add authentication

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,15 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:edit, :update, :show, :like]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update]
+  
 
   def index
     @recipes = Recipe.paginate(page:params[:page], per_page: 4)
   end
   
   def show
-    @recipe = Recipe.find(params[:id])
+
   end
   
   def new
@@ -14,7 +18,7 @@ class RecipesController < ApplicationController
   
   def create
     @recipe = Recipe.new(recipe_params)
-    @recipe.chef = Chef.find(3);
+    @recipe.chef = current_user
     
     if @recipe.save
       flash[:success] = "Your recipe was created successfully!"
@@ -26,13 +30,11 @@ class RecipesController < ApplicationController
   end
   
   def edit
-    @recipe = Recipe.find(params[:id])
+
   end
   
   def update
-    @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
-      #do something
       flash[:success] = "Your recipe was updated successfully!"
       redirect_to  recipe_path(@recipe)
     else
@@ -42,8 +44,7 @@ class RecipesController < ApplicationController
   end
   
   def like
-    @recipe = Recipe.find(params[:id])
-    like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+    like = Like.create(like: params[:like], chef: current_user, recipe: @recipe)
     if(like.valid?)
       flash[:success] = "Your selection was successful"
       redirect_to :back
@@ -58,6 +59,17 @@ class RecipesController < ApplicationController
   
     def recipe_params
       params.require(:recipe).permit(:name, :summary, :description, :picture)
+    end
+    
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+    
+    def require_same_user
+      if current_user != @recipe.chef
+        flash[:danger] = "You can only edit your own recipes"
+        redirect_to recipes_path
+      end
     end
     
 end
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -8,5 +8,5 @@ class Chef < ActiveRecord::Base
   validates :email, presence: true, length: {minimum: 3, maximum: 105 },
                                     uniqueness: {case_sensitive: false},
                                     format: { with: VALID_EMAIL_REGEX }
-                                    
+  has_secure_password                                  
 end
