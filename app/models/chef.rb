fc48545984c1ef0816ce001ce6c018ba59689a6a
delads <don@delads.com>
2016-07-17 10:03:29 +0000

    add pagination

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,6 @@
 class Chef < ActiveRecord::Base
   has_many :recipes
+  has_many :likes
   
   before_save { self.email = email.downcase }
   validates :chefname, presence: true, length: {minimum: 3, maximum: 40 }
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/home', to: 'pages#home'
 
-  resources :recipes 
+  resources :recipes do
+    member do
+      post 'like'
+    end
+  end
+  
+  
 end
