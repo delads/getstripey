e0e1daaa70e8c129ba3962448c737ff00122f639
delads <don@delads.com>
2016-07-20 22:44:17 +0000

    Added delete button to products page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,13 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: [:edit, :update, :show, :like]
+  before_action :set_recipe, only: [:edit, :update, :show, :like, :destroy]
   before_action :require_user, except: [:show, :index]
   before_action :require_same_user, only: [:edit, :update]
   
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success] = "Product deleted"
+    redirect_to recipes_path
+  end
 
   def index
     @recipes = Recipe.paginate(page:params[:page], per_page: 4)
@@ -29,9 +34,7 @@ class RecipesController < ApplicationController
     end
   end
   
-  def edit
 
-  end
   
   def update
     if @recipe.update(recipe_params)
