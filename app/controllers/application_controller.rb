cfbd3879f903012033f41ac65eafe8c905d7ff67
delads <don@delads.com>
2016-08-09 07:13:16 +0000

    Removing protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
   
-  protect_from_forgery :except => :webhook
-  protect_from_forgery :except => :pay
+  #protect_from_forgery :except => :webhook
+  #protect_from_forgery :except => :pay
   
   helper_method :current_user, :logged_in? 
   
